8e075352a35f98683c95f0097a5bc000541f3c60
wieliczk <brandonwieliczko@gmail.com>
2017-10-05 14:17:35 -0600

    python file containing password and username for mysql

---
+++ python/db.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import MySQLdb
+
+hostname = 'localhost'
+username = 'root@localhost'
+password = 'cmput401F3nc1ng'
+database = '401TEST'
+
+
+# Should be able to conenct if everything has been setup correctly
+myConnection = MySQLdb.connect( host=hostname, user=username, passwd=password, db=database )
