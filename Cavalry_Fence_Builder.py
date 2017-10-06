23e1d15f7c2f2c5bba44312a61f5019d37f287bd
Ewong3 <eric3@ualberta.ca>
2017-10-05 22:30:21 -0600

    Added sudo code for authorizing login

--- Cavalry_Fence_Builder.py
+++ Cavalry_Fence_Builder.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Import python files with other functionality such as accessing the database
 #import Python.db as DB
@@ -15,9 +15,29 @@ def showSignUp():
 
 @app.route('/login', methods=['POST'])
 def login():
+    # Get form information
+    username = request.form['username']
+    password = request.form['password']
+    print("Username: " + username)
+    print("Password: " + password)
+    
     # Access MySQL and authorize
     
+    #connection = DB.getConnection()
+    #cursor = connection.cursor()
+    
+    #statement = "SELECT * FROM accounts WHERE Username = '" + username + "' AND Password = '" + password + "'"
+    #numrows = cursor.query(statement)
+    
+    #cursor.close()
+    #connection.close()
+    
     return render_template("home.html")
+
+    #if numrows == 1:
+    #    return render_template("home.html")
+    #else:
+    #    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run()
