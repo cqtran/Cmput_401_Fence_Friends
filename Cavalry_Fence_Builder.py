77bf3392a6218ea8d7a45b8670cb294f15f1adb9
cqtran <cqtran@ualberta.ca>
2017-10-07 19:02:02 -0600

    fixed css for customer

--- Cavalry_Fence_Builder.py
+++ Cavalry_Fence_Builder.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 # Import python files with functionality
-# import Python.accounts as Accounts
+#import Python.accounts as Accounts
 
 app = Flask(__name__) #, template_folder = "HTML", static_folder = "CSS")
 
