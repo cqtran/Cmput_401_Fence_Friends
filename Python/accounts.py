59df81654ab1bcf8f6e307fb8a09566769930912
Ewong3 <eric3@ualberta.ca>
2017-10-09 13:54:53 -0600

    createAccount sqlalchemy code

--- Python/accounts.py
+++ Python/accounts.py
@@ -1,5 +1,5 @@
 import Python.db as DB
-import sqlalchemy import *
+from sqlalchemy import *
 
 def authenticate(username, password):
     # Access MySQL and authenticate the username/password
@@ -8,22 +8,31 @@ def authenticate(username, password):
     metadata = MetaData(db)
     accounts = Table('accounts', metadata, autoload=True)
 	
-	s = accounts.select(and_(accounts.c.Username == username, accounts.c.Password == password))
+    s = accounts.select(and_(accounts.c.Username == username, accounts.c.Password == password))
     rs = s.execute()    
 
     numrows = 0
     for row in rs:
         print(row.Username, row.Password)
         numrows += 1
-	
+        
+    rs.close()
+    
     print(numrows)
     if numrows == 1:
         return True
     return False
 
-def createAccount():
-    # TODO
-    return
+def createAccount(username, email, password):
+    # Access MySQL and add in account
+    db = DB.getConnection()
+    metadata = MetaData(db)
+    accounts = Table('accounts', metadata, autoload = True)
+    
+    i = accounts.insert().values(Username = username, Password = password)
+    i.execute()
+    
+    return True
 
 def requestAccount(username, email, password):
     # TODO
