f3b2602e5bbaa0dd15022bb5a4a2e149eb2382ee
cqtran <cqtran@ualberta.ca>
2017-10-12 13:39:10 -0600

    company 2 test data

--- Python/db.py
+++ Python/db.py
@@ -4,8 +4,12 @@ import sqlalchemy
 def getConnection():
 
     engine = sqlalchemy.create_engine(
+<<<<<<< Updated upstream
     	#change password to your db password and root to your db username
         'mysql+mysqlconnector://root:password@localhost/data'
+=======
+        'mysql ://root:cmput401F3nc1ng@localhost/401TEST'
+>>>>>>> Stashed changes
     )
 
     return engine
