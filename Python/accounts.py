fcf7ce2ec43e2a5e0ac7f834323f6c19d1b911eb
wieliczk <wieliczk@ualberta.ca>
2017-10-12 13:39:21 -0600

    Added getCompany function -BW

--- Python/accounts.py
+++ Python/accounts.py
@@ -37,3 +37,17 @@ def createAccount(username, password, email):
 def requestAccount(username, email, password):
     # TODO
     return
+
+
+def getCompany(username):
+    db = DB.connection()
+    metadata = MetaData(db)
+    accounts = Tables('Accounts', metadata, autoload=True)
+    s = accounts.select(and_ (accounts.c.Username == username, accouts.c.Company_ID))
+    rs = s.execute()
+
+    for row in rs:
+        print(row.Username, row.Company_ID)
+        return Company_ID
+    return False
+
