723653f9dd864b5e071c06f0a69da32bf6268657
cqtran <cqtran@ualberta.ca>
2017-10-12 13:50:45 -0600

    company id works

--- Python/db.py
+++ Python/db.py
@@ -5,7 +5,7 @@ def getConnection():
 
     engine = sqlalchemy.create_engine(
     #change password to your db password and root to your db username
-    'mysql+mysqlconnector://root:password@localhost/data'
+    'mysql+mysqlconnector://root:dragon@localhost/data'
  	)
 
     return engine
--- SQL/testdata1.sql
+++ SQL/testdata1.sql
@@ -1,9 +1,9 @@
 -- Test Data 1
-INSERT INTO Companies (Company_ID, Name, Password, Email) VALUES (1, 'CavalryFence', 'Builder', 'CavalryFence@CavalryFence.com');
+#INSERT INTO Companies (Company_ID, Name, Password, Email) VALUES (1, 'CavalryFence', 'Builder', 'CavalryFence@CavalryFence.com');
 
-INSERT INTO Accounts (Username, Password, Email, Company_ID) VALUES ('Davis', 'Johnson', 'DJohnston@CavalryFence.com', 1);
-INSERT INTO Accounts (Username, Password, Email, Company_ID) VALUES ('Adam', 'Smith', 'ASmith@CavalryFence.com', 1);
-INSERT INTO Accounts (Username, Password, Email, Company_ID) VALUES ('John', 'Doe', 'JDoe@CavalryFence.com', 1);
+#INSERT INTO Accounts (Username, Password, Email, Company_ID) VALUES ('Davis', 'Johnson', 'DJohnston@CavalryFence.com', 1);
+#INSERT INTO Accounts (Username, Password, Email, Company_ID) VALUES ('Adam', 'Smith', 'ASmith@CavalryFence.com', 1);
+#INSERT INTO Accounts (Username, Password, Email, Company_ID) VALUES ('John', 'Doe', 'JDoe@CavalryFence.com', 1);
 
-INSERT INTO Companies (Company_ID, Name, Password, Email) VALUES (2, 'test2', 'test', 'test2@test.com');
+#INSERT INTO Companies (Company_ID, Name, Password, Email) VALUES (2, 'test2', 'test', 'test2@test.com');
 INSERT INTO Accounts (Username, Password, Email, Company_ID) VALUES ('johnny', 'hoe', 'test@test.com', 2);
