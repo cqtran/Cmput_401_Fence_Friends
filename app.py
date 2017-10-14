83df7b6a9a94db2f40ea9c33935fa884f55a1e3b
Greg <gdbaker@ualberta.ca>
2017-10-14 16:06:25 -0600

    removed import of flask-bootstrap

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask_bootstrap import Bootstrap
 from flask_security import Security, login_required, \
      SQLAlchemySessionUserDatastore
 from Python.db import dbSession, init_db
