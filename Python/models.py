00023b3eda225778538367f453af31bac8d67a1b
Ewong3 <eric3@ualberta.ca>
2017-10-16 16:59:01 -0600

    Added db schema into models.py
    - Added db schema into models.py
    - Changed naming convention of tables

--- Python/models.py
+++ Python/models.py
@@ -3,7 +3,7 @@ from flask_security import UserMixin, RoleMixin
 from sqlalchemy import create_engine
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Boolean, DateTime, Column, Integer, \
-                       String, ForeignKey
+                       String, ForeignKey, Numeric, LargeBinary
 
 class RolesUsers(Base):
     __tablename__ = 'roles_users'
@@ -23,6 +23,7 @@ class User(Base, UserMixin):
     email = Column(String(255), unique=True)
     username = Column(String(255))
     password = Column(String(255))
+    company_id = Column('company_id', Integer(), ForeignKey('company.company_id'))
     last_login_at = Column(DateTime())
     current_login_at = Column(DateTime())
     last_login_ip = Column(String(100))
@@ -32,3 +33,47 @@ class User(Base, UserMixin):
     confirmed_at = Column(DateTime())
     roles = relationship('Role', secondary='roles_users',
                          backref=backref('users', lazy='dynamic'))
+    
+class Company(Base):
+    __tablename__ = 'company'
+    company_id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    email = Column(String(255), unique=True)
+
+class Customer(Base):
+    __tablename__ = 'customer'
+    customer_id = Column(Integer, primary_key=True)
+    email = Column(String(255), unique=True)
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+    cellphone = Column(String(20))
+    company_id = Column('company_id', Integer(), ForeignKey('company.company_id'))
+    
+class Status (Base):
+    __tablename__ = 'status'
+    status_id = Column(Integer, primary_key=True)
+    status_name = Column(String(100))
+
+class Project(Base):
+    __tablename__ = 'project'
+    project_id = Column(Integer, primary_key=True)
+    customer_id = Column('customer_id', Integer(), ForeignKey('customer.customer_id'))
+    status_id = Column('status_id', Integer(), ForeignKey('status.status_id'))
+    address = Column(String(100))
+    start_date = Column(DateTime())
+    end_date = Column(DateTime())
+    
+class Quote(Base):
+    __tablename__ = 'quote'
+    quote_id = Column(Integer, primary_key=True)
+    project_id = Column('project_id', Integer, ForeignKey('project.project_id'))
+    quote = Column(Numeric)
+    project_info = Column(LargeBinary)
+    note = Column(String(255))
+    last_modified = Column(DateTime())
+    
+class Material(Base):
+    __tablename__ = 'material'
+    material_id = Column(Integer, primary_key=True)
+    material_name = Column(String(255))
+    cost = Column(Numeric)
