d5f34936d17d9c04fb2e71ce9cc1b3675c9197f9
Greg <gdbaker@ualberta.ca>
2017-10-19 14:39:37 -0600

    added stuff

--- Python/customers.py
+++ Python/customers.py
@@ -10,27 +10,3 @@ def addCustomer(name, email, ph, addr, cname):
     dbSession.commit()
 
     return True
-
-
-
-
-
-
-def displayCustomers(company_id):
-    db = DB.getConnection()
-    metadata = MetaData(db)
-    customers = Table('Customers', metadata, autoload=True)
-    s = customers.select(customers.c.Company_ID == company_id)
-    result = s.execute()
-    listofcustomers = []
-    for row in result:
-        listofcustomers.append(row.First_name)
-    return listofcustomers
-
-def getCustomer(customer_id):
-    db = DB.getConnection()
-    metadata = MetaData(db)
-    customers = Table('Customers', metadata, autoload=True)
-    s = customers.select(customers.c.Customer_ID == customer_id)
-    result = s.fetchone()
-    return result
--- Python/jsonifyObjects.py
+++ Python/jsonifyObjects.py
@@ -5,9 +5,11 @@ class MyJSONEncoder(JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Customer):
             return {
+            	'customer_id': obj.customer_id,
                 'email': obj.email,
                 'first_name': obj.first_name,
+                'last_name': obj.last_name,
                 'cellphone': obj.cellphone,
+                'company_name': obj.company_name,
             }
         return super(MyJSONEncoder, self).default(obj)
-
