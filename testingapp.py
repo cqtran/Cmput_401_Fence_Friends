eecc02e832b3b88d0139a7fd0dbb5bdeb5eaa4c0
Ewong3 <eric3@ualberta.ca>
2017-10-20 10:58:12 -0600

    Unittests

--- testingapp.py
+++ testingapp.py
@@ -2,7 +2,13 @@ import os
 import unittest
  
 from flask import Flask
-	
+from Python.db import dbSession, Base, init_db, fieldExists, engine
+from Python.models import Customer, Project
+
+import Python.accounts as Accounts
+import Python.customers as Customers
+import Python.projects as Projects
+
 app = Flask(__name__)
 
 class TestCase(unittest.TestCase):
@@ -11,13 +17,36 @@ class TestCase(unittest.TestCase):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
+        init_db()
+        for tbl in reversed (Base.meta.sorted_tables):
+            engine.execute(tble.delete())
     
     def tearDown(self):
-        pass
+        dbSession.remove()
     
     def test_assert(self):
         test = 'test'
         assert test != 'test1'
+        
+    def test_addCustomer(self):
+        noCustomerTest = dbSession.query(Customer).all()
+        assert len(noCustomerTest) == 0
+        
+        Customers.addCustomer('Kat', 'Kat@gmail.com', '555-555-5555', 'Fence')
+        oneCustomerTest = dbSession.query(Customer).all()
+        assert len(oneCustomerTest) == 1
+        assert oneCustomerTest[0].serialize == ' '
+        
+    def test_createProject(self):
+        noProjectTest = dbSession.query(Project).all()
+        assert len(noProjectTest == 0)
+        
+        Projects.createProject(1, 'Not Reached', 'Somewhere Ave', 'Fence', 'A fun fencing project')
+        oneProjectTest = dbSession.query(Project).all()
+        assert len(oneProjectTest) == 1
+        
+    def test_savingNote(self):
+        pass
     
 if __name__ == '__main__':
     unittest.main()
