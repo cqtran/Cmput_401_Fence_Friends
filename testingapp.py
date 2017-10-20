96860f3fbace14630e4dad645fd3fba4ab0c5e08
Ewong3 <eric3@ualberta.ca>
2017-10-20 11:14:02 -0600

    naming error

--- testingapp.py
+++ testingapp.py
@@ -19,7 +19,7 @@ class TestCase(unittest.TestCase):
         app.config['DEBUG'] = False
         init_db()
         for tbl in reversed (Base.metadata.sorted_tables):
-            engine.execute(tble.delete())
+            engine.execute(tbl.delete())
             
         newCompany = Company(company_name = "Fence", email = "e@e.c")
         newStatus = Status(status_name = "Not Reached")
