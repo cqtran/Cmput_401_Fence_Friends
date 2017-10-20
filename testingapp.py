fbd5d14ee82ce1fcbb53a8f64b22797e970e3d70
Ewong3 <eric3@ualberta.ca>
2017-10-20 12:11:11 -0600

    UnitTesting comments

--- testingapp.py
+++ testingapp.py
@@ -18,9 +18,12 @@ class TestCase(unittest.TestCase):
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
         init_db()
+
+        # Clear all tables in the database
         for tbl in reversed (Base.metadata.sorted_tables):
             engine.execute(tbl.delete())
 
+        # Insert required info such as companies and status
         newCompany = Company(company_name = "Fence", email = "e@e.c")
         newStatus = Status(status_name = "Not Reached")
         dbSession.add(newCompany)
@@ -28,23 +31,23 @@ class TestCase(unittest.TestCase):
         dbSession.commit()
 
     def tearDown(self):
+        """Clear all tables"""
         for tbl in reversed (Base.metadata.sorted_tables):
             engine.execute(tbl.delete())
         dbSession.remove()
 
-
-    def test_assert(self):
-        test = 'test'
-        assert test != 'test1'
-
     def test_addCustomer(self):
+        # Test if there are no customers
         noCustomerTest = dbSession.query(Customer).all()
         assert len(noCustomerTest) == 0
 
+        # Try adding a customer through Customers api
         Customers.addCustomer('Kat', 'Kat@gmail.com', '555-555-5555', 'Place','Fence')
         oneCustomerTest = dbSession.query(Customer).all()
+        # Test the customer is successfully added
         assert len(oneCustomerTest) == 1
 
+        # Test the information from the found customer
         result = oneCustomerTest[0].serialize
         assert result['first_name'] == 'Kat'
         assert result['email'] == 'Kat@gmail.com'
@@ -55,13 +58,19 @@ class TestCase(unittest.TestCase):
         newCustomer = Customer(customer_id = 1, first_name = 'Kat', email = 'Kat@gmail.com', cellphone = '555-555-5555', company_name = 'Fence')
         dbSession.add(newCustomer)
         dbSession.commit()
+
+        # Test if there are to projects
         noProjectTest = dbSession.query(Project).all()
         assert len(noProjectTest)  == 0
 
+        # Try adding a project through Project api
         Projects.createProject(1, 'Not Reached', 'Somewhere Ave', 'Fence', 'A fun fencing project')
         oneProjectTest = dbSession.query(Project).all()
+
+        # Test if the project is successfully added
         assert len(oneProjectTest) == 1
 
+        # Test the information from the found project
         result = oneProjectTest[0].serialize
         assert result['status_name'] == 'Not Reached'
         assert result['address'] == 'Somewhere Ave'
@@ -73,14 +82,16 @@ class TestCase(unittest.TestCase):
         dbSession.commit()
 
         Projects.createProject(1, 'Not Reached', 'Somewhere Ave', 'Fence', 'A fun fencing project')
-
         oneProjectTest = dbSession.query(Project).all()
         result = oneProjectTest[0].serialize
+
+        # Test if the note is empty
         assert result['note'] == None
 
         Projects.savenote('This is a new note', oneProjectTest[0].project_id)
         oneProjectTest = dbSession.query(Project).all()
 
+        # Test if the not has changed
         result = oneProjectTest[0].serialize
         assert result['note'] == 'This is a new note'
 
