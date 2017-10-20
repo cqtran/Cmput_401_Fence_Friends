43ad5975d0b334854586755e697f0ea30f002c82
Ewong3 <eric3@ualberta.ca>
2017-10-20 11:38:17 -0600

    upload

--- testingapp.py
+++ testingapp.py
@@ -1,6 +1,6 @@
 import os
 import unittest
- 
+
 from flask import Flask
 from Python.db import dbSession, Base, init_db, fieldExists, engine
 from Python.models import Customer, Project, Company, Status
@@ -12,60 +12,60 @@ import Python.projects as Projects
 app = Flask(__name__)
 
 class TestCase(unittest.TestCase):
-    
-    def setUp(self):    
+
+    def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
         app.config['DEBUG'] = False
         init_db()
         for tbl in reversed (Base.metadata.sorted_tables):
             engine.execute(tbl.delete())
-            
+
         newCompany = Company(company_name = "Fence", email = "e@e.c")
         newStatus = Status(status_name = "Not Reached")
         dbSession.add(newCompany)
         dbSession.add(newStatus)
         dbSession.commit()
-    
+
     def tearDown(self):
         dbSession.remove()
-    
+
     def test_assert(self):
         test = 'test'
         assert test != 'test1'
-        
+
     def test_addCustomer(self):
         noCustomerTest = dbSession.query(Customer).all()
         assert len(noCustomerTest) == 0
-        
+
         Customers.addCustomer('Kat', 'Kat@gmail.com', '555-555-5555', 'Place','Fence')
         oneCustomerTest = dbSession.query(Customer).all()
         assert len(oneCustomerTest) == 1
         assert oneCustomerTest[0].serialize == ' '
-        
+
     def test_createProject(self):
-        newCustomer = Customer(1, 'Kat', 'Kat@gmail.com', '555-555-5555', 'Place', 'Fence')
+        newCustomer = Customer(customer_id = 1, first_name = 'Kat', email = 'Kat@gmail.com', cellphone = '555-555-5555', company_name = 'Fence')
         dbSession.add(newCustomer)
         dbSession.commit()
         noProjectTest = dbSession.query(Project).all()
         assert len(noProjectTest)  == 0
-        
+
         Projects.createProject(1, 'Not Reached', 'Somewhere Ave', 'Fence', 'A fun fencing project')
         oneProjectTest = dbSession.query(Project).all()
         assert len(oneProjectTest) == 1
-        
+
     def test_savingNote(self):
-        newCustomer = Customer(1, 'Kat', 'Kat@gmail.com', '555-555-5555', 'Place', 'Fence')
+        newCustomer = Customer(customer_id = 1, first_name = 'Kat', email = 'Kat@gmail.com', cellphone = '555-555-5555', company_name = 'Fence')
         dbSession.add(newCustomer)
         dbSession.commit()
-        
+
         Projects.createProject(1, 'Not Reached', 'Somewhere Ave', 'Fence', 'A fun fencing project')
-        
+
         assert oneProjectTest[0].note == None
         Projects.savenote('This is a new note', 1)
         oneProjectTest = dbSession.query(Project).all()
         assert oneProjectTest[0].note != None
         assert oneProjectTest[0].note == 'This is a new note'
-        
+
 if __name__ == '__main__':
     unittest.main()
