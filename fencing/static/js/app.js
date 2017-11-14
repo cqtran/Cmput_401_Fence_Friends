5a263c735a89fcab8aab68e2204485cdf49e49f5
Greg <gdbaker@ualberta.ca>
2017-11-14 00:49:29 -0700

    searching for projects and customers working

--- fencing/static/js/app.js
+++ fencing/static/js/app.js
@@ -20,34 +20,65 @@ var customers = new Bloodhound({
             });
         }
     },
-    limit: 10
+    limit: 5
+});
+
+// Instantiate the Bloodhound suggestion engine
+var projects = new Bloodhound({
+    datumTokenizer: function (datum) {
+        return Bloodhound.tokenizers.whitespace(datum.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+        wildcard: '%QUERY',
+        url: '/getProjectList?search=%QUERY',
+
+        filter: function (projects) {
+            console.log(projects);
+            // Map the remote source JSON array to a JavaScript object array
+            return $.map(projects, function (project) {
+                return {
+                    value: project.project_name,
+                    proj_id: project.project_id,
+                    address: project.cellphone,
+                    status: project.status_name,
+                };
+            });
+        }
+    },
+    limit: 5
 });
 
 // Initialize the Bloodhound suggestion engine
 customers.initialize();
+projects.initialize();
 
-$('#search-bar .typeahead').typeahead(null, {
+$('#search-bar .typeahead').typeahead({
+    highlight: true
+  },
+  {
+    name: 'customers',
     displayKey: 'value',
     source: customers.ttAdapter(),
     templates: {
-        suggestion: Handlebars.compile("<p style='padding:6px'><b>{{value}}</b> - Phone {{phone}} </p>"),
-        footer: Handlebars.compile("<b>Searched for '{{query}}'</b>")
+        header: Handlebars.compile("<div class='container-fluid'><h3 class='text-green'>Customer</h3><hr class='mt-2'></div>"),
+        suggestion: Handlebars.compile("<div class='container-fluid'><p><b>{{value}}</b> - Phone {{phone}} </p></div>")
+    }
+  },
+  {
+    name: 'projects',
+    displayKey: 'value',
+    source: projects.ttAdapter(),
+    templates: {
+        header: Handlebars.compile("<div class='container-fluid'><h3 class='text-green'>Projects</h3><hr class='mt-2'></div>"),
+        suggestion: Handlebars.compile("<div class='container-fluid'><p><b>{{value}}</b> - Status - {{status}} - Address {{address}} </p></div>")
     }
   }).on('typeahead:selected', function(event, selection) {
   
-  // the second argument has the info you want
-  window.location.href= '/customerinfo?cust_id=' + selection.cust_id + '&status=All'
-
-});
-
-//unknown if needed for duplicates
-/*function getDupChecker(dupChecker) {
-  if (!_.isFunction(dupChecker)) {
-    dupChecker = dupChecker === false ? ignoreDups : standardDupChecker;
+  if(selection.hasOwnProperty('proj_id')){
+    window.location.href= '/projectinfo?proj_id=' + selection.proj_id
   }
-
-  return dupChecker;
-
-  function ignoreDups() { return false; }
-  function standardDupChecker(a, b) { return a.value === b.value; }
-}*/
+  else if(selection.hasOwnProperty('cust_id')){
+    window.location.href= '/customerinfo?cust_id=' + selection.cust_id + '&status=All'
+  }
+});
