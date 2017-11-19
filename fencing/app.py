0691bb3edd8c45aa7261fb133ab6f01a754cf2b4
Greg <gdbaker@ualberta.ca>
2017-11-19 16:52:38 -0700

    removed error log

--- fencing/app.py
+++ fencing/app.py
@@ -68,15 +68,6 @@ app.config['MAIL_PASSWORD'] = 'fencing401'
 app.config['SECURITY_EMAIL_SENDER'] = SENDER_EMAIL
 app.config['MAIL_SUPPRESS_SEND'] = False
 
-if app.debug is not True:   
-    import logging
-    from logging.handlers import RotatingFileHandler
-    file_handler = RotatingFileHandler('python.log', maxBytes=1024 * 1024 * 100, backupCount=20)
-    file_handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    file_handler.setFormatter(formatter)
-    app.logger.addHandler(file_handler)
-
 mail = Mail(app)
 
 # Setup Flask-Security
