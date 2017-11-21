61c4ee5d689c3cf126de05f17666edf1f11be2e4
Ewong3 <eric3@ualberta.ca>
2017-11-21 15:14:12 -0700

    Added some empty functions in prices
    - Added some empty functions in prices
    - Prices sends a get request to getPricesList. This should filter the
    list by the current_user's company
    - Some rough idea of the csv parser is done. Data is only printed and
    not stored due to required discussion on how we should store materials
    and prices

--- fencing/api/materials.py
+++ fencing/api/materials.py
@@ -15,15 +15,15 @@ import csv
 
 materialBlueprint = Blueprint('materialBlueprint', __name__, template_folder='templates')
 
-@materialBlueprint.route('/getPriceList/<company_name>/', methods=['GET'])
+@materialBlueprint.route('/getPriceList/', methods=['GET'])
 #@login_required
 #@roles_required('primary')
-def getPriceList(company_name):
+def getPriceList():
     """ Returns a list of prices to a company for a certain year """
     if request.method == 'GET':
         materials = dbSession.query(Material)
-        # materials = materials.filter(company_name = current_user.company_name)
-        materials = materials.filter(Material.company_name == company_name).all()
+        #materials = materials.filter(Material.company_name == current_user.company_name).all()
+        materials = materials.all()
         if len(materials) == 0:
             return bad_request('No materials and prices were found for this company')
         return jsonify(materials)
@@ -41,10 +41,31 @@ def uploadPrice():
         stream = io.StringIO(priceFile.stream.read().decode("UTF8"), newline=None)
         csv_input = csv.reader(stream)
         print(csv_input)
+        # Clear materials list? This will cause issues for appearances due to ForeignKeys
+        # dbSession.query(Material.company_name == current_user.company_name).delete()
 
+        category = ''
         for row in csv_input:
-            print(row)
-            #TODO: Parse the csv file
+            #TODO: Parse the csv file.
+            if row[2] == 'My Price' and row[4] == 'Pieces in Bundle':
+                # Category
+                print(row)
+                category = row[0]
+            if row[0] != '' and row[2] == '' and row[4] == '':
+                # Category
+                print(row)
+                category = row[0]
+            if row[1].startswith('$') and row[2].startswith('$'):
+                # Material
+                print('MATERIAL: ' + row[0] + ' | ' + row[2] + ' | ' + category + ' | ' + row[5])
+                material_name = row[0]
+                my_price = row[2]
+                note = row[5]
+                # Insert data into db
 
+                # newMaterial = Material()
+                #dbSession.add(newMaterial)
+            # Otherwise, ignore row
+        #dbSession.commit()
         return created_request('Prices were changed')
     return bad_request('Oops')
