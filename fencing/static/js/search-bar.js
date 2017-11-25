be35a2130422fafe58c7b506a18eb027f342d988
ahmad <csahmad@users.noreply.github.com>
2017-11-24 20:07:29 -0700

    Animate menu showing/hiding

--- fencing/static/js/search-bar.js
+++ fencing/static/js/search-bar.js
@@ -5,11 +5,11 @@ function focusSearch() {
 function expandSearch() {
 	document.getElementById("search-bar").classList.remove("collapsed-search");
 	document.getElementById("search-bar").classList.add("expanded-search");
-	document.getElementById("mobile-menu").classList.add("d-none");
+	document.getElementById("mobile-menu").classList.add("hidden-menu");
 }
 
 function collapseSearch() {
 	document.getElementById("search-bar").classList.remove("expanded-search");
 	document.getElementById("search-bar").classList.add("collapsed-search");
-	document.getElementById("mobile-menu").classList.remove("d-none");
+	document.getElementById("mobile-menu").classList.remove("hidden-menu");
 }
