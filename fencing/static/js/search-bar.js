06b15a35126030c663420589380ad3febe06485b
ahmad <csahmad@users.noreply.github.com>
2017-11-24 19:22:44 -0700

    Hide menu button on search expand (and expand wider)

--- fencing/static/js/search-bar.js
+++ fencing/static/js/search-bar.js
@@ -5,9 +5,11 @@ function focusSearch() {
 function expandSearch() {
 	document.getElementById("search-bar").classList.remove("collapsed-search");
 	document.getElementById("search-bar").classList.add("expanded-search");
+	document.getElementById("mobile-menu").classList.add("d-none");
 }
 
 function collapseSearch() {
 	document.getElementById("search-bar").classList.remove("expanded-search");
 	document.getElementById("search-bar").classList.add("collapsed-search");
+	document.getElementById("mobile-menu").classList.remove("d-none");
 }
