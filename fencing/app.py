98a340d880a4035cba849d9815eb55e665c87b61
ahmad <csahmad@users.noreply.github.com>
2017-11-27 18:08:08 -0700

    Added 500 error page

--- fencing/app.py
+++ fencing/app.py
@@ -442,6 +442,10 @@ def deleteAttachment():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description = 'Run Cavalry Fence Builder')
