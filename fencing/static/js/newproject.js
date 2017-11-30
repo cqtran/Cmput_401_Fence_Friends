793e6fe2a1ac072a02dcd430e3d3d244b3ceb0fe
ahmad <csahmad@users.noreply.github.com>
2017-11-30 10:40:58 -0700

    Fixed delete customer showing 'delete project', use fancy confirm discard dialog in add customer, add project

--- fencing/static/js/newproject.js
+++ fencing/static/js/newproject.js
@@ -1,4 +1,5 @@
 var chosenCustomers = [];
+var isDirty = false;
 
 function showMessage(message) {
 	$('#message-text').html(message);
@@ -92,9 +93,34 @@ function confirmDiscard() {
 }
 
 function markDirty() {
+  isDirty = true;
   window.onbeforeunload = confirmDiscard;
 }
 
 function markClean() {
+  isDirty = false;
   window.onbeforeunload = null;
 }
+
+$(document).ready(function() {
+  $("#confirmDiscardSave").click(function() {
+    $("#save").trigger("click");
+  });
+
+  $("a").click(function(event) {
+    var href = $(this).attr("href");
+
+    if (!isDirty || href.startsWith("#") || href == "" || href == null) {
+      return;
+    }
+
+    event.preventDefault();
+
+    $("#confirmDiscardOkay").click(function() {
+      window.onbeforeunload = null;
+      window.location.replace(href);
+    });
+
+    $("#confirmDiscard").modal("show");
+  });
+});
