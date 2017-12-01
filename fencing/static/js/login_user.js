ea4346a2e34e3f761f04962fddedf7b8923bd0e8
ahmad <csahmad@users.noreply.github.com>
2017-12-01 11:03:13 -0700

    Removed admin modal lines, green checkbox

---
+++ fencing/static/js/login_user.js
@@ -0,0 +1,30 @@
+var checked;
+var remember;
+var label;
+
+$(document).ready(function(){
+  checked = false;
+  remember = $("#remember");
+  label = remember.prev();
+  label.append(' <i id="remember-box" class="fa fa-square-o" aria-hidden="true"></i>');
+
+  label.click(function() {
+    remember.click();
+    checked = !checked;
+    var rememberBox = $("#remember-box");
+
+    if (checked) {
+      rememberBox.removeClass("fa-square-o");
+      rememberBox.addClass("fa-check-square");
+      rememberBox.css("color", "#329664");
+    }
+
+    else {
+      rememberBox.removeClass("fa-check-square");
+      rememberBox.addClass("fa-square-o");
+      rememberBox.css("color", "");
+    }
+  });
+
+  label.css("cursor", "pointer");
+})
